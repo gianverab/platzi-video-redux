bebdf0bbbfed1db343caaabfd6940e9b617ac7b0
Giancarlo Vera <gianverab@gmail.com>
2019-08-26 23:42:38 -0500

    add navlink

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Home from '../pages/containers/home';
+import React, { Fragment } from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Home from "../pages/containers/home";
+import Header from "../pages/components/header";
 // import Playlist from './src/playlist/components/playlist';
-import data from '../api.json';
+import data from "../api.json";
 // console.log('Hola mundo!' )
 
-const homeContainer = document.getElementById('home-container')
+const homeContainer = document.getElementById("home-container");
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
 // const holaMundo = <h1>hola Estudiante!</h1>;
-render( <Home data={data} />, homeContainer);
-
+render(
+  <BrowserRouter>
+    <Fragment>
+      <Header />
+      <Home data={data} />
+    </Fragment>
+  </BrowserRouter>,
+  homeContainer
+);
